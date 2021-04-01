b44f2d04667289ece9677acd7fb23e31c1236304
David Mihal <david@dmihal.com>
2021-04-01 17:49:31 +0100

    Use zerion for Uniswap

--- data/adapters/uniswap.ts
+++ data/adapters/uniswap.ts
@@ -1,17 +1,17 @@
 import { OrganizationData } from '../types'
-import { TOKENS, getTokenBalance } from '../utils/tokens'
+import { getPortfolio } from '../utils/zerion'
 
 const vestedContract = '0x1a9c8182c09f50c8318d769245bea52c32be35bc';
 const unvestedContract = '0x4750c43867ef5f89869132eccf19b9b6c4286e1a';
 
 export async function getUniswapData(): Promise<OrganizationData> {
-  const vestedBalance = await getTokenBalance(TOKENS.UNI, vestedContract);
-  const unvestedBalance = await getTokenBalance(TOKENS.UNI, unvestedContract);
+  const vestedBalance = await getPortfolio(vestedContract);
+  const unvestedBalance = await getPortfolio(unvestedContract);
 
   return {
     id: 'uniswap',
     name: 'Uniswap',
     category: 'l1',
-    treasury: (vestedBalance + unvestedBalance) * 30,
+    treasury: vestedBalance + unvestedBalance,
   };
 }

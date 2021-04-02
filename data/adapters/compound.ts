b8ff0acb93a742d904ca7bc9c9d38d77219f04f8
David Mihal <david@dmihal.com>
2021-04-02 13:19:47 +0100

    Add compound resevoir

--- data/adapters/compound.ts
+++ data/adapters/compound.ts
@@ -2,14 +2,16 @@ import { OrganizationData } from '../types'
 import { getPortfolio } from '../utils/zerion'
 
 const treasury = '0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b';
+const resevoir = '0x2775b1c75658Be0F640272CCb8c72ac986009e38';
 
 export async function getCompoundData(): Promise<OrganizationData> {
   const treasuryValue = await getPortfolio(treasury)
+  const resevoirValue = await getPortfolio(resevoir)
 
   return {
     id: 'compound',
     name: 'Compound',
     category: 'l1',
-    treasury: treasuryValue,
+    treasury: treasuryValue + resevoirValue,
   };
 }

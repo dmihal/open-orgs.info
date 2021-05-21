f73eebfecc594f15314216cae2ff16f6265b4e5f
Scott De Taboada <9022895+scottdet@users.noreply.github.com>
2021-05-21 15:36:12 -0700

    pull dg treasury information

--- data/adapters/decentralgames.ts
+++ data/adapters/decentralgames.ts
@@ -1,17 +1,13 @@
 import { OrganizationData } from '../types'
-import { getPortfolio } from '../utils/zerion'
-
-const land = '0x7146cae915f1cd90871ecc69999beffdcaf38ff9';
-const treasury = '0x7a61a0ed364e599ae4748d1ebe74bf236dd27b09';
+import { getDGTreasury } from '../utils/zerion'
 
 export async function getDGData(): Promise<OrganizationData> {
-  const treasuryValue = await getPortfolio(treasury)
-  const landValue = await getPortfolio(land)
+  const treasuryValue = await getDGTreasury()
 
   return {
     id: 'dg',
     name: 'Decentral Games',
     category: 'l1',
-    treasury: treasuryValue + landValue,
+    treasury: treasuryValue,
   };
 }

59c3f4ce50a6ef09071b76fc74977f3970e74443
David Mihal <david@dmihal.com>
2021-05-23 18:10:47 +0100

    Add MKR held by MakerDAO

--- data/adapters/makerdao.ts
+++ data/adapters/makerdao.ts
@@ -1,7 +1,9 @@
-import { OrganizationData } from '../types'
 import { ethers } from 'ethers'
+import { OrganizationData } from '../types'
+import { getPortfolio } from '../utils/zerion'
 
 const VAT_ADDRESS = '0x35d1b3f3d7966a1dfe207aa4514c12a259a0492b'
+const PAUSE_ADDRESS = '0xbe8e3e3618f7474f8cb1d074a26affef007e98fb'
 
 const vatABI = [
   'function dai(address holder) external view returns (uint256)',
@@ -16,10 +18,12 @@ export async function getMakerDAOData(): Promise<OrganizationData> {
   const sin = await vat.sin('0xA950524441892A31ebddF91d3cEEFa04Bf454466')
   const value = ethers.utils.formatUnits(dai.sub(sin), 45)
 
+  const pauseValue = await getPortfolio(PAUSE_ADDRESS)
+
   return {
     id: 'makerdao',
     name: 'MakerDAO',
     category: 'l1',
-    treasury: parseFloat(value),
+    treasury: parseFloat(value) + pauseValue,
   }
 }

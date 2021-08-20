f5dd4d83ee9782be7a1c4b96eb501b1aebf87025
David Mihal <david@dmihal.com>
2021-08-20 16:25:41 +0100

    Uniswap full treasury

--- data/adapters/uniswap.ts
+++ data/adapters/uniswap.ts
@@ -1,17 +1,23 @@
 import { OrganizationData } from '../types'
 import { getPortfolio } from '../utils/zerion'
 
-const vestedContract = '0x1a9c8182c09f50c8318d769245bea52c32be35bc';
-const unvestedContract = '0x4750c43867ef5f89869132eccf19b9b6c4286e1a';
+const treasury = '0x1a9c8182c09f50c8318d769245bea52c32be35bc';
+
+const vestingTreasury = [
+  '0x4750c43867ef5f89869132eccf19b9b6c4286e1a',
+  '0xe3953d9d317b834592ab58ab2c7a6ad22b54075d',
+  '0x4b4e140d1f131fdad6fb59c13af796fd194e4135',
+  '0x3d30b1ab88d487b0f3061f40de76845bec3f1e94',
+];
 
 export async function getUniswapData(): Promise<OrganizationData> {
-  const vestedBalance = await getPortfolio(vestedContract);
-  const unvestedBalance = await getPortfolio(unvestedContract);
+  const balances = await Promise.all([treasury, ...vestingTreasury].map(address => getPortfolio(address)));
+  const totalTreasury = balances.reduce((a: number, b: number) => a + b, 0);
 
   return {
     id: 'uniswap',
     name: 'Uniswap',
     category: 'l1',
-    treasury: vestedBalance + unvestedBalance,
+    treasury: totalTreasury,
   };
 }

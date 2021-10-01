1991d96d2887b2a59ece363643b4ebefec3d14e1
David Mihal <david@dmihal.com>
2021-10-01 01:09:31 +0100

    Disable decentral games

--- data/adapters/index.ts
+++ data/adapters/index.ts
@@ -1,7 +1,7 @@
 import sdk from '../sdk'
 
 import { setup as setupAave } from './aave'
-import { setup as setupDecentralGames } from './decentralgames'
+// import { setup as setupDecentralGames } from './decentralgames'
 import { setup as setupDXdao } from './dxdao'
 import { setup as setupMakerDAO } from './makerdao'
 import { setup as setupPoolTogether } from './pooltogether'
@@ -10,7 +10,7 @@ import { setup as setupSimpleTreasuries } from './simple-treasuries'
 const feesList = sdk.getList('treasuries')
 
 feesList.addAdaptersWithSetupFunction(setupAave)
-feesList.addAdaptersWithSetupFunction(setupDecentralGames)
+// feesList.addAdaptersWithSetupFunction(setupDecentralGames)
 feesList.addAdaptersWithSetupFunction(setupDXdao)
 feesList.addAdaptersWithSetupFunction(setupMakerDAO)
 feesList.addAdaptersWithSetupFunction(setupPoolTogether)

a7495b9bae67a8e23b6402b0acefea69e0b6f1bc
David Mihal <david@dmihal.com>
2022-03-26 23:07:18 +0300

    Use Alchemy for RPC

--- data/sdk.ts
+++ data/sdk.ts
@@ -1,13 +1,19 @@
 import { CryptoStatsSDK } from '@cryptostats/sdk'
 
-if (!process.env.MORALIS_KEY) {
-  console.error('Moralis key missing')
-}
+// if (!process.env.MORALIS_KEY) {
+//   console.error('Moralis key missing')
+// }
 
 const sdk = new CryptoStatsSDK({
-  moralisKey: process.env.MORALIS_KEY,
-  ipfsGateway: 'https://ipfs.cryptostats.community',
-  adapterListSubgraph: 'dmihal/cryptostats-adapter-registry-test',
+  // moralisKey: process.env.MORALIS_KEY,
+  executionTimeout: 60,
 })
+
+if (process.env.ALCHEMY_ETH_KEY) {
+  const rpc = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_KEY}`
+  sdk.ethers.addProvider('ethereum', rpc, { archive: true })
+} else {
+  console.error('Alchemy key not set')
+}
 
 export default sdk
